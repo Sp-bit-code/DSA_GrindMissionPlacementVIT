c56c7fc9f825576adb344e756037c07ca9beb89c
Sparsh Srivastava <sparshsrivastava10d@gmail.com>
2026-09-22 18:29:37 +0530

    Time: 30 ms (70.05%), Space: 144.6 MB (59.01%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // 1. Remove index which is outside the window
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // 2. Remove smaller elements from back
+            while (!dq.isEmpty() &&
+                   nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+
+            // 3. Add current index
+            dq.offerLast(i);
+
+            // 4. Window is ready
+            if (i >= k - 1) {
+                ans[index++] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
