cfedb39f89ecad7b5af66fafed7dc3a19745206d
Sparsh Srivastava <sparshsrivastava10d@gmail.com>
2026-09-23 15:22:09 +0530

    Time: 2 ms (99.4%), Space: 47 MB (59.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,18 @@
+// class Solution{
+//     public int[] twoSum(int[]nums,int target){ 
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){ 
+//             int need=target-nums[i];
+//             if(map.containsKey(need)){ 
+//                 return new int[]{map.get(need),i};
+//             }
+//             map.put(nums[i],i);
+//         }
+//         return new int[]{};
+//     }
+// }
+
+
 class Solution{
     public int[] twoSum(int[]nums,int target){ 
         HashMap<Integer,Integer>map=new HashMap<>();
@@ -7,6 +22,7 @@ class Solution{
                 return new int[]{map.get(need),i};
             }
             map.put(nums[i],i);
+
         }
         return new int[]{};
     }
